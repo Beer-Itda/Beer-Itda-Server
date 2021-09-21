0858f4246d5a5747b402e6ab3026b0b0dc16557f
sssua-0928 <hansua990928@gmail.com>
2021-09-21 17:29:37 +0900

    feat: Style model

---
+++ models/style_mid.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define('Style_Mid', {
+    //중분류
+    mid_name: {
+      type: DataTypes.STRING(45),
+      unique: true,
+      allowNull: false,
+    },
+    //설명
+    description: {
+      type: DataTypes.STRING(45),
+      unique: true,
+      allowNull: false,
+    },
+  }, {
+    //옵션지정
+    freezeTableName: true,
+    timestamps: false,
+  });
+};
