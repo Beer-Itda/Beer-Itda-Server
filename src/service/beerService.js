719a0d8c14eac3f388095e8c7adccfd6fe728833
sssua-0928 <hansua990928@gmail.com>
2021-10-17 03:41:22 +0900

    fix:불필요한 파일삭제 및 폴더 정리

--- src/service/beerService.js
+++ src/service/beerService.js
@@ -3,33 +3,5 @@ const {
 } = require('../../models/index');
 
 module.exports = {
-  // 맥주 세부정보 불러오기
-  beerInformation: async (beer_id) => {
-    try {
-      const beer_Information = await Beer.findOne({
-        attributes: ['id', 'k_name', 'e_name', , 'country', 'star_avg', 'thumbnail_image'],
-        where: {
-          id: beer_id
-        },
-      });
-      return beer_Information;
-    } catch (err) {
-      console.log(err);
-      throw err;
-    }
-  },
-  //전체 맥주 불러오기
-  getAllBeer: async (req, res) => {
-    try {
-      const beers = await Beer.findAll({
-        attributes: ['id', 'k_name', 'e_name', 'star_avg', 'thumbnail_image']
-      });
-      const result = {};
-      result.beers = beers;
-      return res.status(statusCode.OK).send(util.success(responseMessage.BEER_OK, result));
-    } catch (error) {
-      console.error(error);
-      return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.BEER_READ_ALL_FAIL));
-    }
-  },
+  // 계산할 것이 있을 때 사용하기
 }
