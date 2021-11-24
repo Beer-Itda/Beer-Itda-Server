ec7f86ffc6da1d59c41b945993cc8ff9c9b814bd
sssua-0928 <hansua990928@gmail.com>
2021-11-24 15:48:28 +0900

    feat: 스타일, 향 필터

--- src/controller/beer/beerChoiceAromaGET.js
+++ src/controller/beer/beerChoiceAromaGET.js
@@ -12,19 +12,39 @@ const {
 const util = require('../../../modules/util');
 const statusCode = require('../../../modules/statusCode');
 const responseMessage = require('../../../modules/responseMessage');
-
-//const beerService = require("../service/beerService");
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 module.exports = {
-  // 좋아하는 향 beer 전체 불러오기(페이징 안됨) [전체보기 기준, id값 정렬할 것]
+  // 좋아하는 향 beer 전체 불러오기 [전체보기 기준, id값 정렬할 것]
   getAllAromaBeer: async (req, res) => {
     try {
       const beers = await Beer.findAll({
-        attributes: ['id', 'k_name', 'e_name', 'star_avg', 'thumbnail_image']
+        attributes: ['id', 'k_name', 'e_name', 'star_avg', 'thumbnail_image', 'aroma_id_1', 'aroma_id_2'],
+        where: {
+          [Op.or]: [{
+            aroma_id_1: {
+              [Op.or]: [2, 3, 5, 6]
+            },
+            aroma_id_2: {
+              [Op.or]: [2, 3, 5, 6]
+            },
+            /*
+            aroma_id_3: {
+              [Op.or]: [1, 2, 3, 4]
+            },
+            aroma_id_4: {
+              [Op.or]: [1, 2, 3, 4]
+            }
+            */
+          }]
+        },
+        order: [
+          ['id', 'ASC']
+        ]
       });
-      //좋아하는 향 불러오는 로직
       const result = {};
-      result.Stylebeers = beers;
+      result.Aromabeers = beers;
       return res.status(statusCode.OK).send(util.success(responseMessage.BEER_OK, result));
     } catch (error) {
       console.error(error);
--- src/controller/beer/beerChoiceStyleGET.js
+++ src/controller/beer/beerChoiceStyleGET.js
@@ -29,34 +29,6 @@ module.exports = {
           id: user_id,
         }
       });
-      // userSelectStyle와 JSON.stringify(userSelectStyle)의 type
-      console.log(`---userSelectStyle의 type : ${typeof(userSelectStyle)}`);
-      console.log(`---JSON.stringify(userSelectStyle)의 type : ${typeof(JSON.stringify(userSelectStyle))}`);
-
-      console.log(`---JSON.stringify(userSelectStyle) >>> ${JSON.stringify(userSelectStyle)}`);
-      console.log(`---JSON.stringify(userSelectStyle)[0] >>> ${ JSON.stringify((userSelectStyle)[0]) }`);
-      const a = JSON.stringify((userSelectStyle)[0]);
-      const b = (JSON.stringify(userSelectStyle))[2];
-      const c = userSelectStyle.toString();
-      console.log(a, typeof (a), '\n\n', b, typeof (b), '\n\n', c[0], typeof (c));
-
-      /*
-      const abc = Object.values(userSelectStyle);      
-      const objtostr = JSON.stringify(userSelectStyle);
-      console.log(objtostr, '\n\n');
-      */
-
-
-      // 배열의 값 숫자만큼 for문 돌리기
-      var st = new Array();
-      for (var i = 0; i < 5; i++) {
-        if (userSelectStyle[i] != 'null') {
-          st[i] = JSON.stringify(userSelectStyle[i]);
-          console.log(`배열값 ${i} 작성 : ${st[i]}`);
-          //console.log("배열값 작성: ", i, ": ", st[i]);
-        }
-      }
-
 
       const beers = await Beer.findAll({
         attributes: ['id', 'k_name', 'e_name', 'star_avg', 'thumbnail_image', 'style_id'],
@@ -66,7 +38,7 @@ module.exports = {
           }
         }
       });
-      //좋아하는 스타일 불러오는 로직
+
       const result = {};
       result.Stylebeers = beers;
       return res.status(statusCode.OK).send(util.success(responseMessage.BEER_OK, result));
