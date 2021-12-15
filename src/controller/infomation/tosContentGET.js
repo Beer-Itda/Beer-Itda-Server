3592ebade1ca1ed58634f2c39d60ff32a0af7b30
jaehwaii <ypmc91@gmail.com>
2021-12-16 01:03:32 +0900

    init ToS 관련 API

---
+++ src/controller/infomation/tosContentGET.js
@@ -0,0 +1,14 @@
+const statusCode = require('../../../modules/statusCode');
+
+module.exports = {
+  getEachContent: async (req, res) => {
+    try {
+
+    } catch (error) {
+      console.log(error);
+      return res.json(
+        error
+      )
+    }
+  }
+}
--- src/routes/v1/infomation.js
+++ src/routes/v1/infomation.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const styleListAllGET = require('../../controller/infomation/styleListAllGET');
 const aromaListAllGET = require('../../controller/infomation/aromaListAllGET');
+const tosContentGET = require('../../controller/infomation/tosContentGET');
 const router = express.Router();
 
 /* GET users listing. */
 router.get('/style', styleListAllGET.getAllStyleList);
 router.get('/aroma', aromaListAllGET.getAllAromaList);
+router.get('/ToS/:contents', tosContentGET.getEachContent);
 
 module.exports = router;
