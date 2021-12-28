3b6a78d49c6e4d9136edb64508f3381e69117d9b
jaehwaii <ypmc91@gmail.com>
2021-12-29 02:50:02 +0900

    마이 리뷰 데이터 로직 및 출력 완성

--- src/controller/review/reviewMyAllGET.js
+++ src/controller/review/reviewMyAllGET.js
@@ -1,4 +1,4 @@
-const { Review } = require("../../../models")
+const { Review, Beer } = require("../../../models");
 
 module.exports = {
   //나의 페이지에서 내가 작성한 모든 리뷰를 불러온다.
@@ -18,12 +18,45 @@ module.exports = {
           code: "BEER_REVIEW_ERROR",
           message: "리뷰를 불러오는 중 에러가 발생했습니다."
         });
+      //맥주 데이터와 리뷰 데이터를 합쳐서 전체 출력
+      const reuslt_my_review = await review_plus_beer(my_review);
 
       //출력된 데이터 모두를 시간 내림 차순으로 출력한다.
-      return res.json(my_review);
+      return res.json({
+        'my_reivew': reuslt_my_review
+      });
     } catch (error) {
       console.log(error);
       return res.json(error);
     }
   }
 };
+
+async function review_plus_beer(my_review) {
+  const all_my_review = [];
+  for (let i = 0; i < my_review.length; i++) {
+    const beer_id = my_review[i].beer_id;
+    const beer_data_by_review = await find_beer(beer_id);
+
+    const add_beer_review_data = {
+      'beer': beer_data_by_review,
+      'review': my_review[i]
+    };
+    all_my_review.push(add_beer_review_data);
+  };
+  return all_my_review;
+};
+
+async function find_beer(beer_id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const result = Beer.findOne({
+        where: {
+          id: beer_id
+        },
+        raw: true
+      });
+      resolve(result)
+    }, 5)
+  })
+}
