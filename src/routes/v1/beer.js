6713f0f84936797cd08d6fd3b1b0cd3cc702bd9e
sua <72644361+sssua-0928@users.noreply.github.com>
2022-01-02 11:55:22 +0900

    fix: 찜한 맥주 불러오기 페이징

--- src/routes/v1/beer.js
+++ src/routes/v1/beer.js
@@ -6,6 +6,7 @@ const beerDetailGET = require('../../controller/beer/beerDetailGET');
 const beerMonthlyGET = require('../../controller/beer/beerMonthlyGET');
 const beerRandomGET = require('../../controller/beer/beerRandomGET');
 const beerHeartGET = require('../../controller/beer/beerHeartGET');
+const beerTestGET = require('../../controller/beer/beerTestGET');
 const router = express.Router();
 
 const jwtModule = require('../../../modules/jwt');
@@ -17,6 +18,7 @@ router.get('/monthly', beerMonthlyGET.getMonthlyBeer);
 router.get('/random', beerRandomGET.getRandomBeer);
 router.get('/style', jwtModule.checkAuth, beerChoiceStyleGET.getAllStyleBeer);
 router.get('/aroma', jwtModule.checkAuth, beerChoiceAromaGET.getAllAromaBeer);
-router.get('/heart', jwtModule.checkAuth, beerHeartGET.getAllHeartBeer);
+router.get('/heart/:cursor', jwtModule.checkAuth, beerHeartGET.getAllHeartBeer);
+router.get('/test', jwtModule.checkAuth, beerTestGET.getTestBeer);
 
 module.exports = router;
