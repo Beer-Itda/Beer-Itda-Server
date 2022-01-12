b2ed2052be54e9d2b118bb1108b0835e7d781ad2
jaehwaii <ypmc91@gmail.com>
2022-01-12 23:50:17 +0900

    검색 controller에 pagination service 추가

--- src/controller/information/searchBeerBreweryGET.js
+++ src/controller/information/searchBeerBreweryGET.js
@@ -1,17 +1,20 @@
 const { Beer } = require("../../../models");
 const sequelize = require('sequelize');
+const informationService = require("../../service/informationService");
+const { informationServie } = require("../../service");
 const Op = sequelize.Op;
 
 module.exports = {
   searchEveryBeerBrewery: async (req, res) => {
-    const word = req.query.word;
-    const limit = parseInt(req.query.limit);
+    const { page, size, word } = req.query;
 
     if (!word)
       return res.json({
         code: "NEED_SEARCHING_WORD",
         message: "검색어가 필요합니다."
       });
+
+    const { limit, offset } = await informationService.get_pagination(page, size);
 
     Beer.findAndCountAll({
       where: {
@@ -29,13 +32,17 @@ module.exports = {
         ]
       },
       limit: limit,
+      offset: offset,
       order: [
         ['k_name', 'ASC'],
         ['e_name', 'ASC']
-      ]
-    }).then(result => {
-      console.log(result)
-      return res.json(result);
+      ],
+      raw: true
+    }).then(async result => {
+
+      const response = await informationServie.get_paging_data(result, page, limit);
+
+      return res.json(response);
     }).catch(error => {
       console.log(error);
       return res.json(error);
