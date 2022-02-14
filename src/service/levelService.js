33974b42544e97b3b930d73e25a4990bb47c71dd
jaehwaii <ypmc91@gmail.com>
2022-02-15 00:25:33 +0900

    유저 관련 리뷰/레벨 계산 로직 개선 - level_service로 계산 로직 이전 - 기존 등급 가이드 출력 및 user info api에 적용

--- src/service/levelService.js
+++ src/service/levelService.js
@@ -1,3 +1,4 @@
+const {Level, User} = require("../../models");
 module.exports = {
     calc_user_review_level: async(review_count, req, res) => {
         try{
@@ -12,6 +13,44 @@ module.exports = {
                 return level_id = 4;
             else
                 return level_id = 5;
+        } catch (error){
+            console.log(error);
+            return res.json(error);
+        }
+    },
+    calc_user_review_and_level: async(user_id, req, res) => {
+        try{
+            //level 전체
+            const levels = await Level.findAll({raw: true});
+            //user 데이터
+            const user = await User.findOne({
+                attributes: ['review_count', 'level_id'],
+                where: {
+                    id: user_id
+                }, raw: true
+            });
+            //유저 레벨
+            const user_current_level = await Level.findOne({
+                attributes: ['id', 'level', 'level_count'],
+                where: {
+                    id: user.level_id
+                }, raw: true
+            });
+            //유저 다음 레벨 계산
+            const next_level_data = levels.find(level_data => level_data.id === (user.level_id + 1));
+            //남은 리뷰 갯수 계산
+            const need_review_count = next_level_data.level_count - user.review_count;
+            //current_level - 유저 현재 레벨
+            //need_review_count - 다음 리뷰까지 필요한 리뷰 갯수
+            //next_level - 유저 다음 레벨
+            const user_result_data = {};
+            user_result_data.current_level_id = user_current_level.id;
+            user_result_data.current_review_count = user.review_count;
+            user_result_data.current_level = user_current_level.level;
+            user_result_data.need_review_count = need_review_count;
+            user_result_data.next_level = next_level_data.level;
+
+            return user_result_data;
         } catch (error){
             console.log(error);
             return res.json(error);
