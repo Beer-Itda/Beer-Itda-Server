b53fbba61561b0600b6767aca905f8804db13fd6
sua <72644361+sssua-0928@users.noreply.github.com>
2022-02-18 17:33:10 +0900

    fix: 사용하지 않는 로직 삭제

--- src/routes/v1/heart.js
+++ src/routes/v1/heart.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const heartAllGET = require('../../controller/heart/heartAllGET');
 const heartPOST = require('../../controller/heart/heartPOST');
 const router = express.Router();
 
 const jwtModule = require('../../../modules/jwt');
 
 /* GET users listing. */
-router.get('/', jwtModule.checkAuth, heartAllGET.getAllHeart);
 router.post('/', jwtModule.checkAuth, heartPOST.postHeart);
 
 module.exports = router;
